3d7d5cf4eccce5d81245f36ee5f32a3599d801fb
Stussyz <mfadhol07@gmail.com>
2026-04-03 09:56:35 +0700

    update data object "engineer", callback with map

--- src/App.tsx
+++ src/App.tsx
@@ -5,55 +5,75 @@ import HeaderComp from './components/HeaderComp.tsx'
 import ProfileCard from "./components/ProfileCard.tsx";
 
 type engineer = {
-  name: string;
-  year: number;
-  job: string;
+  Name: string;
+  Birth: number;
+  Job: string;
+  Id: number;
 };
 
 const engineers : engineer[] = [
   {
-    name : "ado",
-    year : 2000,
-    job : "software engineer"
+    Name : "ado",
+    Birth : 2000,
+    Job : "software engineer",
+    Id : 1
   },
   {
-    name : "fujisaki",
-    year : 1994,
-    job : "mechanical engineer"
+    Name : "fujisaki",
+    Birth : 1994,
+    Job : "mechanical engineer",
+    Id : 2
   },
   {
-    name : "leonardo",
-    year : 1997,
-    job: "marine engineer"
+    Name : "leonardo",
+    Birth : 1997,
+    Job : "marine engineer",
+    Id : 3
   }
-    ,
     ];
 
+// component
 function App() {
   // mereturn sebuah component jsx
   return (
-    // bisa pakai tag <div> | <></> | <Fragment></Fragment>
-    // inline style profileCard
-    <div style = {{display: "flex",flexDirection: "row", gap: "5px",}}>
-      <HeaderComp />
-      <h1>Learning Typescript</h1>
-      {/* memanggil komponen yg ada di Welcome.tsx */}
-      {/* menggunakan self closing tag pada Welcome */}
+  <>
+    <HeaderComp />
+    <h1>Learning Typescript</h1>
+    {/* bisa pakai tag <div> | <></> | <Fragment></Fragment> */}
+    {/* inline style profileCard */}
+    <div style = {{display: "flex",flexDirection: "row", gap: "5px", justifyContent: "center", alignItems: "center"}}>
+
       {/* <ProfileCard Name="Ado" Job="Junior Programmer" Birth={2000}/>
       <ProfileCard Name="Fadhol" Job="Soldier" Birth={2000}/>
       <ProfileCard Name="Dara" Job="Architect" Birth={2000}/> */}
 
       {/* pemanggilan value menggunakan array */}
-      {[
+      {/* {[
         <ProfileCard Name="Ado" Job="Junior Programmer" Birth={2000}/>,
         <ProfileCard Name="Fadhol" Job="Soldier" Birth={2000}/>,
         <ProfileCard Name="Dara" Job="Architect" Birth={2000}/>
-      ]};
-      
+        ]}; */}
 
-      <Welcome />
+      {/* BEST WAY: */}
+      {engineers.map((engineer) => {
+        return (
+          <ProfileCard
+            Name={engineer.Name}
+            Birth={engineer.Birth}
+            Job={engineer.Job}
+            // Key untuk membedakan ProfileCard 1 dgn yg lain & mempermudah rendering react
+            Key={engineer.Id}
+          />
+        );
+      })}
     </div>
-  )
+
+    {/* memanggil komponen yg ada di Welcome.tsx */}
+    {/* menggunakan self closing tag pada Welcome */}
+    <Welcome />
+  </>
+  );
 }
+
 // di export sebagai "App" untuk pemanggilan
 export default App;
