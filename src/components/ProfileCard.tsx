a45345f4a89480041486ab4a5e7cdf3d0afb4b04
Stussyz <mfadhol07@gmail.com>
2026-04-03 08:07:38 +0700

    update best way of using props

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 type ProfileCardType = {
-    Name: string;
+    Name?: string;
     Birth: number;
     // for optional value
     Job?: string;
@@ -9,14 +9,23 @@ const ProfileCard = (props: ProfileCardType) => {
     // const Name = "Leonardo";
     // const Birth = "3 August 2000";
     // const Job = "Software Engineer";
+
+    // BEST WAY: 
+    const {Name, Birth, Job} = props
     
     return (
         // inline styling jsx itu bentuknya OBJECT bukan string!
         // kalau html itu bentuknya baru STRING!
         <div className = "card">
-            <p>Name: {props.Name}</p>
-            <p>Birth: {props.Birth}</p>
-            {props.Job ? <p>Job: {props.Job}</p>: null}
+            {/* kalau tanpa default value "Name" di const ProfileCard, maka bisa bikin DV nya sperti ini:*/}
+            {/* <p>Name: {Name || "Arema"} </p> */}
+            <p>Name: {Name}</p>
+            <p>Birth Year: {Birth}</p>
+            {/* IF-Ternary (operator) di "Job" itu gini casenya: */}
+            {/* {Job ? <p>Job: {Job}</p>: null} */}
+            
+            {/*  BEST WAY: kalau tanpa props 1/1 di object, maka bisa di call seperti ini: */}
+            {Job && <p>Job: {Job}</p>}
         </div>
     );
 };
