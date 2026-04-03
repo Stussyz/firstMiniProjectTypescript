edb454ae404a500b64b3f63d297995627abba0de
Stussyz <mfadhol07@gmail.com>
2026-04-03 08:06:03 +0700

    update data "engineers" and showing data with array of component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,53 @@ import Welcome from './components/Welcome.tsx'
 import HeaderComp from './components/HeaderComp.tsx'
 import ProfileCard from "./components/ProfileCard.tsx";
 
+type engineer = {
+  name: string;
+  year: number;
+  job: string;
+};
+
+const engineers : engineer[] = [
+  {
+    name : "ado",
+    year : 2000,
+    job : "software engineer"
+  },
+  {
+    name : "fujisaki",
+    year : 1994,
+    job : "mechanical engineer"
+  },
+  {
+    name : "leonardo",
+    year : 1997,
+    job: "marine engineer"
+  }
+    ,
+    ];
+
 function App() {
   // mereturn sebuah component jsx
   return (
     // bisa pakai tag <div> | <></> | <Fragment></Fragment>
-    <div style = {{display: "flex", gap: "5px",}}>
+    // inline style profileCard
+    <div style = {{display: "flex",flexDirection: "row", gap: "5px",}}>
       <HeaderComp />
       <h1>Learning Typescript</h1>
       {/* memanggil komponen yg ada di Welcome.tsx */}
       {/* menggunakan self closing tag pada Welcome */}
-      <ProfileCard Name="Ado" Job="Junior Software Engineer" Birth={2000}/>
-      <ProfileCard Name="Fadhol" Job="Lecturer" Birth={2000}/>
-      <ProfileCard Name="Dara" Job="" Birth={2000}/>
+      {/* <ProfileCard Name="Ado" Job="Junior Programmer" Birth={2000}/>
+      <ProfileCard Name="Fadhol" Job="Soldier" Birth={2000}/>
+      <ProfileCard Name="Dara" Job="Architect" Birth={2000}/> */}
+
+      {/* pemanggilan value menggunakan array */}
+      {[
+        <ProfileCard Name="Ado" Job="Junior Programmer" Birth={2000}/>,
+        <ProfileCard Name="Fadhol" Job="Soldier" Birth={2000}/>,
+        <ProfileCard Name="Dara" Job="Architect" Birth={2000}/>
+      ]};
+      
+
       <Welcome />
     </div>
   )
