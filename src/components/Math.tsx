d7c6319c6244bf99ce818780c97bf7b169f2bf39
Stussyz <mfadhol07@gmail.com>
2026-04-06 15:53:47 +0700

    import useState to using hook n state, fixing increment + decrement counter logic function, and add resetCounter

--- src/components/Math.tsx
+++ src/components/Math.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 const Math = () => {
+    // useState = hook untuk menyimpan state
+    // count = state untuk menyimpan nilai counter
+    // setCount = function untuk mengubah nilai count (default valuenya)
+    const [count, setCount] = useState(0); 
+
+    // function untuk handle increment, decrement, dan reset counter
     const handleIncrement = () => {
-        alert("ini penjumlahan")
+        // alert("ini penjumlahan");
+        setCount(count + 1);
     }
     const handleDecrement = () => {
-        alert("ini pengurangan")
+        // alert("ini pengurangan");
+        setCount(count - 1);
     }
-    
+    const resetCounter = () => {
+        // alert("Counter di reset");
+        setCount(0);
+    }
+
 return (
     <div style={{ display: "flex", gap: "5px",}}>
         <button onClick={handleIncrement}>Tambah</button>
-        <p>0</p>
+        <p>{count}</p>
         <button onClick={handleDecrement}>Kurang</button>
+        <button onClick={resetCounter}>Reset</button>
     </div>
 )
 };
